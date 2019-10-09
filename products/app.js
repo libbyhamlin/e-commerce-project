a628ed8bdb22cddebb85d21cb9a6af028ba2deab
Libby Hamlin <bravolibby@gmail.com>
2019-10-08 21:41:21 -0700

    updated files created app.js with function

--- products/app.js
+++ products/app.js
@@ -1,2 +1,11 @@
 //Put for loop here
+import sneakers from '../sneakerData.js';
+import sneakerRender from './sneaker-render.js';
 
+const list = document.getElementById('sneakers');
+
+for (let i = 0; i < sneakers.length; i++) {
+    const sneaker = sneakers[i];
+    const dom = sneakerRender(sneaker);
+    list.appendChild(dom);
+}
