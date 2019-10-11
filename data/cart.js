bf9e7dd1a8d5c4d2acb7d7a3fc990739946d5f2a
Libby Hamlin <bravolibby@gmail.com>
2019-10-10 17:56:34 -0700

    created product order array

---
+++ data/cart.js
@@ -0,0 +1,28 @@
+const productOrder = [
+    {
+        id: 'airJordan8',
+        quantity: 2
+    },
+    {
+        id: 'airJordan6',
+        quantity: 1
+    },
+    {
+        id: 'airJordan13',
+        quantity: 1
+    }
+];
+
+export default productOrder;
+
+const emptyArray = [];
+
+const 123Array = [1, 2, 3];
+
+const obj123Array = [{'num': 1}, {'num': 2}, {'num': 3}]; 
+
+
+const emptyObject = {};
+const 1Object = {'num': 1};
+const 2Object = {'num': 2};
+const 3Object = {'num': 3};
